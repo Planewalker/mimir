3b6921ca09d4ec2229bc1cc1da21cf9945681079
Bastlynn <bastlynn@gmail.com>
2021-12-29 02:42:18 -0500

    Last base updates

--- sites/all/themes/planewalker/page.tpl.php
+++ sites/all/themes/planewalker/page.tpl.php
@@ -80,7 +80,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language; ?>" xml:lang="<?php print $language->language; ?>">
 
-<head><base href="/Users/bastlynn/Desktop/PLANEWALKER-BACKUP/mimir.planewalker.com/site/">
+<head><base href="https://planewalker.com">
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
   <?php print $styles; ?>
